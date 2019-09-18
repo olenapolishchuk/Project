13cd8c7bc75b56a0018c4136623604c807f62e2c
olenapolishchuk <48185242+olenapolishchuk@users.noreply.github.com>
2019-09-19 00:15:38 +0300

    Add  methods1.h

---
+++ Project/methods1.h
@@ -0,0 +1,25 @@
+#pragma once
+const int max_is_prime = 20000;
+bool is_prime(int n);
+int m_is_prime();
+void methods1(int t);
+
+
+
+
+
+int met_1_int();
+double met_1();
+
+int met_2_int();
+double met_2();
+
+int met_3_int();
+double met_3();
+
+int inv(int x);
+int met_4_int();
+double met_4();
+
+int met_5_int();
+double met_5();

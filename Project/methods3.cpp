9637d788f2a37bd0bf53df7da45707e095a73d0b
olenapolishchuk <48185242+olenapolishchuk@users.noreply.github.com>
2019-09-19 07:57:36 +0300

    modified methods3.cpp

--- Project/methods3.cpp
+++ Project/methods3.cpp
@@ -10,19 +10,19 @@
 
 static const double m = 10;
 
-double rand9() {
-	double u = met_1();
+double met_9() {
+	double u = met_4();
 	double x = -m * log(u);
 	return x;
 }
 static const double a = 10;
 
-double rand10() {
+double met_10() {
 	double u = met_1();
 	double y = tan(M_PI*u);
 	double x = sqrt(2 * a - 1)*y + a - 1;
-	if (x <= 0) return rand10();
+	if (x <= 0) return met_10();
 	double v = met_4();
-	if (v > (1 + y * y)*exp((a - 1)*log(x / (a - 1)) - sqrt(2 * a - 1)*y)) return rand10();
+	if (v > (1 + y * y)*exp((a - 1)*log(x / (a - 1)) - sqrt(2 * a - 1)*y)) return met_10();
 	return x;
 }
